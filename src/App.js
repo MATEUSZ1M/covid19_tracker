5e1ee0fc48ede3be4b036bf8bb52421d31ae7640
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-12-13 22:09:30 +0100

    Add funcionality to InfoBox

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import "./App.css";
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -33,8 +34,22 @@ function App() {
     const countryCode = event.target.value;
     setCountry(countryCode);
 
-    console.log("country code: ", countryCode);
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+
+        //all of the data from the country response
+        setCountryInfo(data);
+      });
   };
+
+  console.log(countryInfo);
 
   return (
     <div className="app">
@@ -59,9 +74,9 @@ function App() {
         </div>
 
         <div className="app__stats">
-          <InfoBox title="Coronavirus Cases" cases={123} total={2000} />
-          <InfoBox title="Recovered" cases={1233} total={3000} />
-          <InfoBox title="Deaths" cases={1234} total={4000} />
+          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
         </div>
 
         <Map />
